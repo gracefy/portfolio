18ad190e0b782ab0fe570e5f1693689ed2af973e
gracefy <grace1188y@gmail.com>
2025-09-27 13:23:48 -0400

    Add linkdein link to hero section

--- src/components/hero/HeroText.jsx
+++ src/components/hero/HeroText.jsx
@@ -5,7 +5,7 @@ import { BiSolidRightTopArrowCircle } from 'react-icons/bi';
 import { HiMiniSparkles } from 'react-icons/hi2';
 import { SiGithub } from 'react-icons/si';
 
-import { FaGithub, FaRobot } from 'react-icons/fa';
+import { FaGithub, FaRobot, FaLinkedin } from 'react-icons/fa';
 import { MdSmartToy } from 'react-icons/md';
 
 // Hero text component with framer motion
@@ -63,6 +63,24 @@ const HeroText = () => {
         >
           <FaGithub className="w-5 h-5" />
           <span className="group-hover:transition-all group-hover:duration-500">View GitHub</span>
+          <BiSolidRightTopArrowCircle className="w-4 h-4 group-hover:transition-all group-hover:duration-500" />
+        </a>
+      </motion.div>
+
+      <motion.div
+        variants={fadeIn('left', 0.8)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0 }}
+        className="mx-auto md:mx-0 mt-2"
+      >
+        <a
+          href="https://www.linkedin.com/in/gracefengye/"
+          target="_blank"
+          className="group flex gap-1 items-center text-md text-lime-600 dark:text-lime-300 underline cursor-pointer hover:text-lime-400 dark:hover:text-lime-500"
+        >
+          <FaLinkedin className="w-5 h-5" />
+          <span className="group-hover:transition-all group-hover:duration-500">View LinkedIn</span>
           <BiSolidRightTopArrowCircle className="w-4 h-4 group-hover:transition-all group-hover:duration-500" />
         </a>
       </motion.div>
